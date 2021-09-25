12395bee4dfaa64e9795c9f87e2141a02c4fdb1a
Jessica Wise <jwise1596@knights.ucf.edu>
2021-09-25 11:35:36 -0400

    3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -16,6 +16,7 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
@@ -33,7 +34,7 @@ namespace Zork
                     case Commands.LOOK:
                         Console.WriteLine(CurrentRoom.Description);
                         break;
-
+                    
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
@@ -51,10 +52,6 @@ namespace Zork
             }
         }
 
-        private static Commands ToCommand(string commandString) => (Enum.TryParse(commandString, true, out Commands result) ?result) : Commands.UNKNOWN;
-
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
         private static bool Move(Commands command)
         {
             Assert.IsTrue(IsDirection(command), "Invalid Direction.");
@@ -71,7 +68,7 @@ namespace Zork
                     break;
 
                 case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Locaton.Column++;
+                    Location.Column++;
                     break;
 
                 case Commands.WEST when Location.Column > 0:
@@ -86,6 +83,12 @@ namespace Zork
             return isValidMove;
         }
 
+        private static Commands ToCommand(string commandString) => 
+            Enum.TryParse(commandString, true, out Commands result) ?result : Commands.UNKNOWN;
+
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
+        
+
         private static readonly Room[,] Rooms = {
             { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
             { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
@@ -94,9 +97,28 @@ namespace Zork
 
         private static void InitializeRoomDescriptions()
         {
+            Rooms[0,0].Description = "You are on a rock-strewn trail."; // Rocky Trail
+            Rooms[0,1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred."; // South of House
+            Rooms[0,2].Description = "You are at the top of the Great Canyon on its south wall."; // Canyon View
 
+            Rooms[1,0].Description = "This is a forest, with trees in all directions around you."; //Forest
+            Rooms[1,1].Description = "This is an open field west of the white house, with a baorded front door."; //West of House
+            Rooms[1,2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; //Behind House
+
+            Rooms[2,0].Description = "This is dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; //Dense Woods
+            Rooms[2,1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; //North of House
+            Rooms[2,2].Description = "You are in a clearing, with a forest surrounding you on the west and south."; //Clearing
         }
 
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
         private static (int Row, int Column) Location = (1, 1);
+        
     }  
 }
--- Zork/Room.cs
+++ Zork/Room.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Zork
+﻿namespace Zork
 {
     class Room
     {
         public string Name { get; }
 
         public string Description { get; set; }
-        public Room(string name, string descripton = "")
+        public Room(string name, string description = "")
             //This is a constructor initializes name and description so that it can never be changed
         {
             Name = name;
             Description = description;
         }
+        public override string ToString() => Name;
     }
 }
